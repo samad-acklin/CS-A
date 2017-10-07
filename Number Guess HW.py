68908fc5fb977ec76055700d0c745e66845bb3ac
samad-acklin <samad.acklin@smash.lpfi.org>
2017-10-07 12:22:20 -0700

    Number Guess (HW)

---
+++ Number Guess HW.py
@@ -0,0 +1,16 @@
+age=input("how old are you?")
+if int(age) > 18:
+    guess_age = input("you're are an adult")
+
+correct_age = "19"
+guess_age = input("I'm thinking of a age between 1 and 20. Guess it!\n")
+while guess_age != correct_age:20
+if guess_age < correct_age:20
+print("Too low. Try again!")
+
+print("Too hgh.Try again")
+guess_age = int(input(20))
+print("You got it! Congratulations!")
+
+else:
+print("Sorry, that's incorrect. Try Again!")
